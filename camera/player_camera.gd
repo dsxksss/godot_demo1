d847579c6405a4f5d2c7f50c8f2357cfe8c4fcc0
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-04-11 23:20:14 +0800

    优化相机代码逻辑

--- camera/player_camera.gd
+++ camera/player_camera.gd
@@ -1,17 +1,17 @@
 extends Camera2D
 
-var target_position = Vector2.ZERO
+# 相机聚焦至玩家的移动速度
+const CAMERA_MOVE_SPEED = 2
 
 func _ready() -> void:
 	# 成为当前的活动相机
 	make_current()
 
 func _process(delta: float) -> void:
-	acquire_target()
 	# 平滑移动视角
-	global_position = global_position.lerp(target_position,1.0 - exp(-delta * 20))
+	global_position = global_position.lerp(get_player_position(),1.0 - exp(-delta * CAMERA_MOVE_SPEED))
 	
 # 视角移动
-func acquire_target():
+func get_player_position() -> Vector2:
 	var player = $%player as Node2D
-	target_position =player.global_position
+	return player.global_position
